c17f835e5df2235019d22598bf942fe7287c8009
Andrea <andreajaninaescobar@gmail.com>
2017-08-04 16:56:44 -0600

    Cambios en clases

--- Lab#3_AndreaEscobar/src/lab/pkg3_andreaescobar/Pueblo.java
+++ Lab#3_AndreaEscobar/src/lab/pkg3_andreaescobar/Pueblo.java
@@ -1,10 +1,12 @@
 
 package lab.pkg3_andreaescobar;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Pueblo {
     
     private String pueblo;
+    private ArrayList<Integrantes> integ = new ArrayList();
 
     public Pueblo() {
     }
@@ -21,10 +23,20 @@ public class Pueblo {
         this.pueblo = pueblo;
     }
 
+    public ArrayList<Integrantes> getInteg() {
+        return integ;
+    }
+
+    public void setInteg(ArrayList<Integrantes> integ) {
+        this.integ = integ;
+    }
+
     @Override
     public String toString() {
-        return "Pueblo{" + "pueblo=" + pueblo + '}';
+        return "Pueblo{" + "pueblo=" + pueblo + ", integ=" + integ + '}';
     }
+    
+    
     
     
 }
