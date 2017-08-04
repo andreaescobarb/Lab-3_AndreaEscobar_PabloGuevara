bc898eaef76a9c91407d463e89a60127bde31b9d
Pablo-Guevara <pronaldogp@gmail.com>
2017-08-04 15:23:53 -0600

    Pueblos

--- Lab#3_AndreaEscobar/src/lab/pkg3_andreaescobar/Lab3_AndreaEscobar.java
+++ Lab#3_AndreaEscobar/src/lab/pkg3_andreaescobar/Lab3_AndreaEscobar.java
@@ -15,9 +15,11 @@ public class Lab3_AndreaEscobar {
             System.out.println("1-Creacion Pueblo");
             System.out.println("2-Modificar Pueblo");
             System.out.println("3-Eliminar Pueblo");
-            System.out.println("4-Crear Personaje");
-            System.out.println("5-Modificar Personaje");
-            System.out.println("6-Elminar personaje");
+            System.out.println("4-Mostrar Pueblos");
+            System.out.println("5-Crear Personaje");
+            System.out.println("6-Modificar Personaje");
+            System.out.println("7-Elminar personaje");
+            System.out.println("8-Motrar Personajes");
             System.out.println("Ingrese su opcion: ");
             int op = sc.nextInt();
             switch (op) {
